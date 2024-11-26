39bd87607d3cfafd688d69243f861f94c2d2ad4d
melekelmoula <162648449+melekelmoula@users.noreply.github.com>
2024-11-26 19:21:35 +0100

    Update index.js

--- my-node-backend/index.js
+++ my-node-backend/index.js
@@ -1,35 +1,31 @@
 const express = require('express');
 const cors = require('cors');
-const routes = require('./routes/approutes');  // Import the routes file
+const routes = require('./routes/approutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Dynamic CORS setup
 const allowedOrigins = [
-  'http://localhost:3000', // Local development
-  'https://tunimarket.vercel.app' // Production
+  'http://localhost:3000', 
+  'https://tunimarket.vercel.app'
 ];
 
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true); // Allow the request
+      callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  //credentials: true, // If using cookies or auth headers
+  credentials: true,
   optionsSuccessStatus: 200
 }));
 
-// Middleware
-app.use(express.json()); // Middleware to parse JSON request bodies
-
-// Use the routes defined in approutes.js
+app.use(express.json());
 app.use(routes);
 
-// Start server
 app.listen(PORT, () => {
-  console.log(`Server is running on ${process.env.BASE_URL || `http://localhost:${PORT}`}`);
+  console.log(`Server running at ${process.env.BASE_URL || `http://localhost:${PORT}`}`);
 });
