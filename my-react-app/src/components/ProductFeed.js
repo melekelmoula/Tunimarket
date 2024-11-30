0b5a1da1c76bb50193ee94c7e82eb833505be370
melekelmoula <162648449+melekelmoula@users.noreply.github.com>
2024-12-01 00:20:38 +0100

    Update ProductFeed.js

--- my-react-app/src/components/ProductFeed.js
+++ my-react-app/src/components/ProductFeed.js
@@ -1,11 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import ProductDetail from './ProductDetail';
 import { Helmet } from 'react-helmet';
+import { useLocation } from 'react-router-dom';
 
 function ProductFeed({ filteredProducts, handleProductClick }) {
   const [visibleProducts, setVisibleProducts] = useState([]);
   const [productsToShow, setProductsToShow] = useState(8);
   const [selectedProduct, setSelectedProduct] = useState(null);
+  const location = useLocation(); // Hook to get the current URL
+  const categories = {
+    autoparts: 'Autoparts - Find the best auto parts and accessories at TuniMarket.',
+    clothes: 'Clothes - Shop for stylish clothes and accessories on TuniMarket.',
+    technology: 'Technology - Explore the latest gadgets and tech products on TuniMarket.'
+  };
+
+  const categoryKeywords = {
+    autoparts: 'TuniMarket, autoparts Tunisia, car accessories, buy auto parts, auto repair, online marketplace Tunisia',
+    clothes: 'TuniMarket, clothes Tunisia, fashion Tunisia, buy clothes, stylish clothes, online shopping Tunisia',
+    technology: 'TuniMarket, technology Tunisia, buy gadgets, electronics Tunisia, tech products, online shopping Tunisia'
+  };
 
   useEffect(() => {
     setVisibleProducts(filteredProducts.slice(0, productsToShow));
@@ -28,43 +41,80 @@ function ProductFeed({ filteredProducts, handleProductClick }) {
     return <ProductDetail product={selectedProduct} handleBack={() => setSelectedProduct(null)} />;
   }
 
+  const currentUrl = location.pathname === '/' 
+  ? 'https://tunimarket.vercel.app' 
+  : `https://tunimarket.vercel.app${location.pathname}`;
+
+
+  const getCategoryName = () => {
+    // Get category from URL (e.g., /category/Clothes)
+    const pathParts = location.pathname.split('/');
+    const category = pathParts[2]; // 'Clothes', 'Autoparts', 'Technology'
+    return category || 'Home'; // Default to 'Home' if no category is found
+  };
+  
+  const getCategoryDescription = () => {
+    // Get category from URL (e.g., /category/Clothes)
+    const pathParts = location.pathname.split('/');
+    const category = pathParts[2]; // 'Clothes', 'Autoparts', 'Technology'
+  
+    // Check if the category exists in the URL and return the corresponding description
+    // If no category is found, return the default description for the homepage
+    if (category && categories[category.toLowerCase()]) {
+      return categories[category.toLowerCase()];  // Matches categories like 'Clothes', 'Autoparts', 'Technology'
+    }
+  
+    return 'Discover the best deals for buying and selling on TuniMarket, the online marketplace of Tunisia.'; // Default description for homepage or invalid category
+  };
+
+  const getCategoryKeywords = () => {
+    // Get category from URL (e.g., /category/Clothes)
+    const pathParts = location.pathname.split('/');
+    const category = pathParts[2]; // 'Clothes', 'Autoparts', 'Technology'
+
+    // Check if the category exists and return the corresponding keywords
+    // If no category is found, return the default set of keywords
+    return category && categoryKeywords[category.toLowerCase()]
+      ? categoryKeywords[category.toLowerCase()]
+      : 'TuniMarket, Tunisia, online shopping, buy and sell Tunisia, marketplace Tunisia';
+  };
+
+  
   return (
     <div>
       {/* Meta tags for SEO */}
       <Helmet>
-        <meta name="description" content="Discover the best deals for buying and selling on TuniMarket, the online marketplace of Tunisia." />
-        <meta name="keywords" content="TuniMarket, Tunisia, online shopping, buy and sell Tunisia, marketplace Tunisia" />
-        <meta property="og:title" content="TuniMarket - Buy and Sell in Tunisia" />
-        <meta property="og:description" content="The best site to buy and sell in Tunisia. Explore exclusive products on TuniMarket." />
-        <meta property="og:url" content={`https://tunimarket.vercel.app`} />
+        <meta name="description" content={getCategoryDescription()} />
+        <meta name="keywords" content={getCategoryKeywords()} />
+        <title>{`TuniMarket - ${getCategoryName() === 'Home' ? 'Buy and Sell in Tunisia' : getCategoryName()}`}</title>
         <meta name="robots" content="index, follow" />
-        <link rel="canonical" href={`https://tunimarket.vercel.app`} />
+        <meta property="og:url" content={currentUrl}/>
+        <meta property="og:type" content="website"/>
+        <link rel="canonical" href={currentUrl} />
         <link rel="preload" href="/assets/fonts/your-font.woff2" as="font" type="font/woff2" crossorigin="anonymous" />
 
-       {/* Structured data (JSON-LD) for product listings */}
-<script type="application/ld+json">
-  {`
-    {
-      "@context": "https://schema.org",
-      "@type": "Product",
-      "name": "TuniMarket - Marketplace Products",
-      "description": "Browse top products on TuniMarket.",
-      "brand": "TuniMarket",
-      "offers": {
-        "@type": "Offer",
-        "url": "https://tunimarket.vercel.app/product/1JsA6Gp37F0JOoXZgY8P",  // Replace with dynamic product URL
-        "priceCurrency": "TND",
-        "price": "100",  // Replace with dynamic price
-        "priceValidUntil": "2024-12-31",
-        "availability": "https://schema.org/InStock",
-        "hasMerchantReturnPolicy": {
-          "@type": "MerchantReturnPolicy",
-          "url": "https://tunimarket.vercel.app/returns"  // Replace with your return policy URL
+     {/* Structured data (JSON-LD) for product listings */}
+     <script type="application/ld+json">
+      {`
+        {
+          "@context": "https://schema.org",
+          "@type": "Organization",
+          "name": "TuniMarket - ${getCategoryName() === 'Home' ? 'Marketplace Products' : getCategoryName()}",  // Dynamic name based on category
+          "description": "${getCategoryDescription()}",
+          "brand": "TuniMarket",
+          "mainEntityOfPage": {
+            "@type": "WebPage",
+            "@id": "${currentUrl}"
+          },
+          "url": "${currentUrl}",
+          "logo": "https://tunimarket.vercel.app/Sample.jpg",
+          "hasMerchantReturnPolicy": {
+            "@type": "MerchantReturnPolicy",
+            "url": "https://tunimarket.vercel.app/returns"
+          }
         }
-      }
-    }
-  `}
-</script>
+      `}
+    </script>
 
 
         {/* Preload images */}
@@ -73,27 +123,29 @@ function ProductFeed({ filteredProducts, handleProductClick }) {
         ))}
       </Helmet>
 
-          <div className="row mt-4">
+      <div className="row mt-4">
         {/* Mapping over filtered and visible products */}
         {visibleProducts.map((product) => (
           <article key={product.id} className="col-md-4 mb-4" onClick={() => handleProductClick(product)}>
             <figure className="card h-100 shadow-sm">
               {/* Product image */}
               <img
-                src={product.imageUrl}
-                alt={product.name}
-                className="card-img-top"
-                style={{ objectFit: 'cover' }}
-                loading="eager" // Improve loading performance
-                  width="300" // Fixed width for LCP optimization
-          height="200" // Fixed height for LCP optimization
-          sizes="(max-width: 768px) 100vw, 33vw" // Responsive image size
-          priority 
+               priority="true"
+
+               src={product.imageUrl}
+               alt={product.name}
+               className="card-img-top"
+               style={{ objectFit: 'cover' }}
+               loading="eager" // Improve loading performance
+               width="300" // Fixed width for LCP optimization
+               height="200" // Fixed height for LCP optimization
+               sizes="(max-width: 768px) 100vw, 33vw" // Responsive image size
+ 
               />
               <figcaption className="card-body text-center">
                 {/* Product name */}
                 <h5 className="card-title">{product.name}</h5>
-                <h7 className="card-title">{product.price + ' TND'}</h7>
+                <p className="card-title">{product.price + ' DT'}</p>
               </figcaption>
             </figure>
           </article>
